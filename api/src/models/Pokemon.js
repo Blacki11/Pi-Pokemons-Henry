4fc1fcf50fab3990b141f4f4bff1ea476c5c84c6
Blacki11 <jeremiasescuela@gmail.com>
2023-03-11 21:28:28 -0300

    Models Pokemon 0.0.1

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,40 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
+//--OPCIONALES--
+// Velocidad.
+// Altura.
+// Peso.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id:{
+      // Testeo pokemon con "uuid" y type sin este, si uuid no da problemas, tambien cambio el de type
+      // type: DataTypes.INTEGER,
+      // autoIncrement: true,
+      // primaryKey: true
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    life: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
   });
 };
