b49a0eeb92fcbdcfa1f6c0fe801e80b9614696f9
Blacki11 <jeremiasescuela@gmail.com>
2023-03-15 01:37:42 -0300

    React-Redux v0.0.1

--- client/src/pages/inicio/inicio.jsx
+++ client/src/pages/inicio/inicio.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import React from "react";
 
-export default function Inicio(){
-    return (
-        <div>
-            <button>
-                <Link to='home'>Bienvenido</Link>
-            </button>
-        </div>
-    )
+export default function Inicio() {
+  return (
+    <div
+      style={{
+        backgroundImage: `url("https://via.placeholder.com/500")`,
+      }}
+    >
+      <button>
+        <Link to="home">Bienvenido</Link>
+      </button>
+    </div>
+  );
 }
